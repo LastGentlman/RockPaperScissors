787995b06b0eccc42b1d9695bbf2c33578e23cb7
Rodrigo <gokusan@keemail.me>
2022-01-30 23:06:52 -0600

    Latest Changes (Unespected Result)

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+const paperBtn = document.getElementById('1')
+                .addEventListener('click', playGround('paper'));
+const scissorsBtn = document.getElementById('2')
+                .addEventListener('click', playGround('scissors'));
+const rockBtn = document.getElementById('3')
+                .addEventListener('click', playGround('rock'));
+
+const iaSelection = document.querySelector('ia-sel');
+
 function aiPlay() {
     let choices = ['Rock', 'Paper', 'Scissors'];
     let choice = choices[Math.floor(Math.random() * choices.length)];
@@ -6,29 +15,8 @@ function aiPlay() {
     return choice;
 }
 
-function playGround(pla, ai) {
-    pla = prompt("What's ur choise?");
-    if (pla === 'Rock' && ai === 'Paper') {
-        console.log('You have lost :(');
-    } else if (pla === 'Paper' && ai === 'Scissors') {
-        console.log('You have lost :(');
-    } else if (pla === 'Scissors' && ai === 'Rock') {
-        console.log('You have lost :(');
-    } else if (pla === 'Rock' && ai === 'Rock'){
-        console.log('Its a Draw :/');
-    } else if (pla === 'Paper' && ai === 'Paper') {
-        console.log('Its a Draw :/');
-    } else if (pla === 'Scissors' && ai === 'Scissors') {
-        console.log('Its a Draw :/');
-    } else if (pla === 'Rock' && ai === 'Scissors'){
-        console.log('U Win!! :)');
-    }  else if (pla === 'Paper' && ai === 'Rock'){
-        console.log('U Win!! :)');
-    }  else if (pla === 'Scissors' && ai === 'Paper'){
-        console.log('U Win!! :)');
-    } else {
-        console.log('Unespected Error');
-    }
+function playGround(pla) {
+   console.log(`${pla} clicked!`);
 }
 
 function game() {
